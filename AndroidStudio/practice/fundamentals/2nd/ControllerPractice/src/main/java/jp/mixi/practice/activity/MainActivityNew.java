36fffcb5ec83ba5656897299583e1d8dcb71eb87
tsatotsato <t_sato@lkc.co.jp>
2020-11-12 15:08:17 +0900

    課題

---
+++ AndroidStudio/practice/fundamentals/2nd/ControllerPractice/src/main/java/jp/mixi/practice/activity/MainActivityNew.java
@@ -0,0 +1,13 @@
+package jp.mixi.practice.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityNew extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_new);
+    }
+}
