0b9816f0fd66501b6c9a983499e066c1352a1fc9
tsatotsato <t_sato@lkc.co.jp>
2020-12-08 18:12:11 +0900

    課題3

--- AndroidStudio/assignments/fundamentals/2nd/ControllerAssignment/src/main/java/jp/mixi/assignment/controller/Fragment2.java
+++ AndroidStudio/assignments/fundamentals/2nd/ControllerAssignment/src/main/java/jp/mixi/assignment/controller/Fragment2.java
@@ -6,10 +6,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class Fragment1 extends Fragment {
+public class Fragment2 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_controller3_fragment1, container, false);
+        return inflater.inflate(R.layout.activity_controller3_fragment2, container, false);
     }
 }
