5ac35f5d3a736ab7a736f10d775e5bae9c0b24f2
tsatotsato <t_sato@lkc.co.jp>
2021-01-13 15:28:47 +0900

    Revert "実習1～3"
    This reverts commit 36b28306d64928203c4bdce5df8b5ccc0463fb74.

--- AndroidStudio/practice/fundamentals/3rd/ResourceManagement/app/src/main/java/jp/mixi/practice/resourcemanagement/StringPractice1And2Activity.java
+++ AndroidStudio/practice/fundamentals/3rd/ResourceManagement/app/src/main/java/jp/mixi/practice/resourcemanagement/StringPractice1And2Activity.java
@@ -2,7 +2,6 @@ package jp.mixi.practice.resourcemanagement;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
 
 public class StringPractice1And2Activity extends AppCompatActivity {
 
@@ -10,11 +9,6 @@ public class StringPractice1And2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.string_practice1);
-
-        String str = getString(R.string.toast_message, "君", 10000);
-        TextView textview = (TextView) findViewById(R.id.textView6);
-        textview.setText(str);
-
     }
 
 }
