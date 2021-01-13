6b54f9a66ebdfec7cef85329655d2eec2e315f34
tsatotsato <t_sato@lkc.co.jp>
2021-01-13 19:07:59 +0900

    実習

--- AndroidStudio/practice/fundamentals/3rd/ResourceManagement/app/src/main/java/jp/mixi/practice/resourcemanagement/AnimationPracticeActivity.java
+++ AndroidStudio/practice/fundamentals/3rd/ResourceManagement/app/src/main/java/jp/mixi/practice/resourcemanagement/AnimationPracticeActivity.java
@@ -1,7 +1,10 @@
 package jp.mixi.practice.resourcemanagement;
 
+import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 /**
  * Created by takafumi.nanao on 2015/02/05.
@@ -11,5 +14,19 @@ public class AnimationPracticeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.animation_practice);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        ImageView imageview = (ImageView) findViewById(R.id.imageView);
+        TransitionDrawable transitionDrawable = (TransitionDrawable) imageview.getBackground();
+
+        transitionDrawable.setCrossFadeEnabled(true);
+        transitionDrawable.startTransition(10000);
+        transitionDrawable.reverseTransition(15000);
+        //transitionDrawable.resetTransition();
+
     }
 }
